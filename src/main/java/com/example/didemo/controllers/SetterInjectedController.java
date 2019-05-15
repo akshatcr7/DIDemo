10bbb32a8b1ceb5c1fdd802342aebe62a5e8ba8d
akshatcr7 <imakshat147@gmail.com>
2019-05-15 15:53:56 +0530

    Dependency Injection with Spring Framework.

--- src/main/java/com/example/didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/example/didemo/controllers/SetterInjectedController.java
@@ -1,16 +1,20 @@
 package com.example.didemo.controllers;
 
 import com.example.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService){
         this.greetingService = greetingService;
     }
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
